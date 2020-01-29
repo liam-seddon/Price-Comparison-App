bfbb007892e8317ef132f7ce6e3b752b60db3fff
liam-seddon <liamseddon@talktalk.net>
2020-01-29 13:41:01 +0000

    rewrote code for toggle

--- js/Javascript.js
+++ js/Javascript.js
@@ -1,8 +1,9 @@
-function ShowPassword() {
-  var x = document.getElementbyId('userpasswd');
-  if(x.type === "password"){
-    x.type = "text";
+function Toggle(e) {
+  if(e.innerHTML == 'Show') {
+    e.innerHTML = 'Hide'
+    document.getElementById('userpasswd').type="text";
   } else {
-    x.type ="password";
+    e.innerHTML ='Show'
+    document.getElementById('userpasswd').type="password"
   }
 }

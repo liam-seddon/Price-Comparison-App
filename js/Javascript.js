b615acbe93f17f7f9154edf30dc9194519e2e511
liam-seddon <liamseddon@talktalk.net>
2020-01-29 12:45:09 +0000

    indented javascript code

--- js/Javascript.js
+++ js/Javascript.js
@@ -1,8 +1,8 @@
 function ShowPassword() {
   var x = document.getElementbyId('userpasswd');
-if(x.type === "password"){
-  x.type = "text";
-} else {
-  x.type ="password";
-}
+  if(x.type === "password"){
+    x.type = "text";
+  } else {
+    x.type ="password";
+  }
 }

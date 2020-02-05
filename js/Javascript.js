8e45f5e99a4a958eadd0add71d988be3c65626d6
liam-seddon <liamseddon@talktalk.net>
2020-02-05 11:14:45 +0000

    added javascript link to shopping area page, added Java code to java file, for Sticky nav bar

--- js/Javascript.js
+++ js/Javascript.js
@@ -1,8 +1,27 @@
+
 function Toggle() {
   var PasswordVis = document.getElementById('userpasswd');
   if (PasswordVis.type ==="password"){
     PasswordVis = "text";
   } else{
     PasswordVis="password"
+  }
+}
+
+
+
+
+window.onscroll = function() {Mynavbar()};
+
+var Sticky_nav = document.getElementById('navbar');
+
+var sticky = navbar.offsetTop;
+
+function]= Mynavbar(){
+  if (window.pageYOffset >= 'sticky') {
+    navbar.ClassList.add("sticky")
+
+  } else {
+    navbar.ClassList.remove("sticky");
   }
 }

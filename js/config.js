0f54e1e0e3a6a5b3378a1a4c6566c1ca28fc8003
liam-seddon <liamseddon@talktalk.net>
2020-03-07 01:05:36 +0000

    finished config file

--- js/config.js
+++ js/config.js
@@ -3,5 +3,15 @@ var connection = mysql.createConnection({
   host: "eu-cdbr-west-02.cleardb.net",
   user: "b609bd1be3d6c0",
   password: "53470153",
-  database: "Price_Comparison_App_DB" 
-})
+  database: "Price_Comparison_App_DB"
+});
+
+connection.connect(function(err){
+  if (!err {
+    console.log("Database Is Connected");
+  } else {
+    console.log("Error Connecting To The Database");
+  }
+});
+
+module.exports = connection;

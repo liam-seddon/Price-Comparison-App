0268cc8e5422f795f03240d3e10357febb39fddb
liam-seddon <liamseddon@talktalk.net>
2020-03-07 01:02:36 +0000

    creating config file to connect to database

---
+++ js/config.js
@@ -0,0 +1,7 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: "eu-cdbr-west-02.cleardb.net",
+  user: "b609bd1be3d6c0",
+  password: "53470153",
+  database: "Price_Comparison_App_DB" 
+})

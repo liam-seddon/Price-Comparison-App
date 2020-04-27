cf2e2ed1be61839f4274fc8e7c12cc5019a23934
liam-seddon <liamseddon@talktalk.net>
2020-04-27 04:03:36 +0100

    added new server

---
+++ server.js
@@ -0,0 +1,22 @@
+//settign variable to HHTP
+var http = require("http");
+var fs =require("fs");
+var body-parser=require("body-parser");
+
+var connection = require("js/config.js");
+
+
+
+http.createServer (function (request, response){
+  //Sends a http header
+  //Returns HTTP status as 200 which is okay
+
+  //writing to our server
+  resposne.wrtieHead(200,{
+    'Content-Type': 'text/html',
+    'Access-Control-Allow-Origin' : '*'
+  });
+  var readStream = fs.readStream(__dirname + '/Index.html');
+  //Send a Message
+  readStream.pipe(response);
+}).listen(PORT);

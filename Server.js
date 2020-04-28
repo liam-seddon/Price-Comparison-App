315a28c1b9f4b186e15244bbe173c52998bd66ee
liam-seddon <liamseddon@talktalk.net>
2020-04-28 15:06:18 +0100

    added html extension

--- Server.js
+++ Server.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + 'Index.html'));
 
 app.get ('/', function(request, response){
-  response.render ('Index');
+  response.render ('Index.html');
 });
 
 app.listen(app.get('port'), function(){

ad28f4b8aa117e308fb846f36cfe20a077137b49
liam-seddon <liamseddon@talktalk.net>
2020-04-28 01:01:56 +0100

    added new code for server.js

--- Server.js
+++ Server.js
@@ -1,1 +1,14 @@
+var express = require('express');
+var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get ('/', function(request, response){
+  response.render ('/Index.html');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Price Comparison App is running', app.get('port') )
+});

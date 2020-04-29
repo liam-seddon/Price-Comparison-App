dfdbd2c2402f34e4f897f019c8dec020feccc0fb
liam-seddon <liamseddon@talktalk.net>
2020-04-29 05:03:43 +0100

    moved server code into main faile called "app.js"

---
+++ app.js
@@ -0,0 +1,35 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var connection = mysql.createConnection({
+  host : 'localhost',
+  user : 'root',
+  password: '',
+  database : 'users'
+});
+
+connection.connect();
+
+global.db =connect();
+
+//Setting Up Enviroments
+app.set('port', (process.env.PORT || 8080));
+
+app.use(express.static(__dirname + '/'));
+
+app.set('/', __dirname + "/index");
+app.set('view engine', 'ejs');
+
+app.get ('/', function(request, response){
+  response.render ('index');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Price Comparison App is running', app.get('port') )
+});
+
+app.get('/', routes.index); //Call My Index PAGE
+app.get('/', routes.Register); //Call My Index PAGE

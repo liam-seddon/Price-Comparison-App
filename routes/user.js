96fa9c5dfc4747f7a33cf735ebe3cdc8a2506b7f
liam-seddon <liamseddon@talktalk.net>
2020-05-04 22:00:53 +0100

    checking to see if databse is connect

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,24 @@
 var express = require("express");
 var router = express.Router();
-var db = require('../config/db.config.js');
+var db = require('../models/db.js');
 
-router.get ('/Shopping_Area', (req, res ,next){
-  var sql = 'SELECT Price, Supplier FROM products_fruit';
-  db.query(sql, function,(err, daat, fields)){
-    if (err) throw err;
-    res.render('Shopping_Area', {title:Shopping Area, product_data_fruit:data});
-  });
-
+router.get('/', function, req, res, next){
+  res.send('Connected';)
+}
 });
+
+
+
+
+
+
+//.get ('/Shopping_Area', (req, res ,next){
+//  var sql = 'SELECT Price, Supplier FROM products_fruit';
+  //db.query(sql, function,(err, daat, fields)){
+    //if (err) throw err;
+  //  res.render('Shopping_Area', {title:Shopping Area, product_data_fruit:data});
+  //});
+
+//});
 
 module.exports = router;

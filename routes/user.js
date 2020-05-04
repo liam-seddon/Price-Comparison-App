a6e06fb60f340dae526b44a6d73da7bb68309dc7
liam-seddon <liamseddon@talktalk.net>
2020-05-04 18:41:45 +0100

    changing about to display data from SQl table to Shopping Area pagw

--- routes/user.js
+++ routes/user.js
@@ -1,85 +1,14 @@
-//Get Homepage
-exports.index = function (request, response){
-  message='';
-  if (req.method == "POST"){
-    //post the data
-  } else {
-    res.render('login');
-  }
-};
+var express = require("express");
+var router = express.Router();
+var db = require('../config/db.config.js');
 
-//Login method
-exports.login = function(request, response){
-   var message = '';
-   var sess = req.session;
+router.get ('/Shopping_Area', (req, res ,next){
+  var sql = 'SELECT Price, Supplier FROM products_fruit';
+  db.query(sql, function,(err, daat, fields)){
+    if (err) throw err;
+    res.render('Shopping_Area', {title:Shopping Area, procut_data_fruit:data});
+  });
 
-   if(req.method == "POST"){
-      var post  = req.body;
-      var name= post.user_name;
-      var pass= post.password;
+});
 
-      var sql="SELECT id, first_name, last_name, user_name FROM `users` WHERE `user_name`='"+name+"' and password = '"+pass+"'";
-      db.query(sql, function(err, results){
-         if(results.length){
-            req.session.userId = results[0].id;
-            req.session.user = results[0];
-            console.log(results[0].id);
-            res.redirect('/views/Shopping_Area');
-         }
-         else{
-            message = 'Wrong Credentials.';
-            res.render('login.ejs',{message: message});
-         }
-
-      });
-   } else {
-      res.render('login.ejs',{message: message});
-   }
-};
-
-
-//Register Method
-exports.signup = function(request, response){
-   message = '';
-   if(req.method == "POST"){
-      var post  = req.body;
-      var name= post.user_name;
-      var pass= post.password;
-      var fname= post.first_name;
-      var lname= post.last_name;
-      var mob= post.mob_no;
-
-      var sql = "INSERT INTO `users`(`first_name`,`last_name`,`mob_no`,`user_name`, `password`) VALUES ('" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass + "')";
-
-      var query = db.query(sql, function(err, result) {
-
-         message = "Succesfully! Your account has been created.";
-         res.render('Register.ejs',{message: message});
-      });
-
-   } else {
-      res.render('Register');
-   }
-};
-
-//Calling Shopping Area method
-exports.Shopping_Area = function(request, response, next){
-
-	var user =  req.session.user,
-	userId = req.session.userId;
-
-	if(userId == null){
-		res.redirect("/home/login");
-		return;
-	}
-
-	 var sql="SELECT * FROM `login_details` WHERE `id`='"+userId+"'";
-
-	   db.query(sql, function(err, results){
-
-		   console.log(results);
-
-		   res.render('Shopping_Area.ejs', {user:user});
-
-		});
-};
+module.exports = router;

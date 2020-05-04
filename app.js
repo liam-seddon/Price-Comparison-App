c93a83c97dbdb8de35f21fe8b0ac1e5f2645193d
liam-seddon <liamseddon@talktalk.net>
2020-05-04 17:46:53 +0100

    deleted db connection

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 //Creating Session Management
-var session = require('express-session');
-app.use(session({
-  secret: '',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 60000 }
-}))
+//var session = require('express-session');
+//app.use(session({
+  //secret: '',
+  //resave: false,
+  //saveUninitialized: true,
+  //cookie: { maxAge: 60000 }
+/}))
 
 //Storing Session VALUES
-var sess = req.session;  //initialize session variable
-req.session.userId = results[0].id; //set user id
-req.session.user = results[0];//set user name
+//var sess = req.session;  //initialize session variable
+//req.session.userId = results[0].id; //set user id
+//req.session.user = results[0];//set user name
 
 //Getting Session Value
-var userId = req.session.userId;
+//var userId = req.session.userId;
 
 //Destroying Session VALUES
-req.session.destroy(function(err) {
-   })
+//req.session.destroy(function(err) {
+  // })
 
 //Setting vars
 var mysql = require('mysql');
@@ -28,18 +28,6 @@ var user = require('./routes/user');
 var http = require('http');
 
 
-//Creating Database Connection
-var connection = mysql.createConnection({
-  host : 'localhost',
-  user : 'root',
-  password: '',
-  database : 'users'
-});
-
-connection.connect();
-
-global.db =connect();
-
 //Setting Up Enviroments / Server
 app.set('port', (process.env.PORT || 8080));
 app.set('views',__dirname +'/views');
@@ -47,7 +35,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 
 
